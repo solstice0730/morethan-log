e049540fba72b5378242d62588de03b013673f3e
solstice <166244272+solstice0730@users.noreply.github.com>
2026-06-30 10:42:40 +0900

    Update profile information in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "이동준",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "Backend / Data Engineering Student",
+    bio: "데이터 엔지니어를 목표로 공부하고 있습니다.",
+    email: "dlthfwns3@naver.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    github: "https://github.com/solstice0730",
+    instagram: "https://www.instagram.com/soljjuni/",
   },
   projects: [
     {
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "꼬마해병",
+    description: "공부 기록을 남기는 블로그",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
